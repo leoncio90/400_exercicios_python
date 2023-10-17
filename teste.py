da89fa7967830ca5a2eabee9c2a3d7a2ef141c27
Jhonata Leoncio <jhonataleoncio@gmail.com>
2023-10-16 22:08:20 -0300

    commit ex.35

---
+++ teste.py
@@ -0,0 +1,10 @@
+num1 = input('Digite o primeiro valor: ')
+num2 = input('Digite o segundo valor: ')
+
+
+if num1 > num2:
+    print('O segundo valor é maior.')
+elif num1 == num2:
+    print('Os valores são iguais.')
+elif num1 > num2:
+    print('o primeiro valor é maior')
